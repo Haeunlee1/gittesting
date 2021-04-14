1564f9d36d192b2b7d665b1aa2693a314ed4eb51
dl434 <dl434@LAPTOP-N1USPNPQ>
2021-04-14 11:11:07 +0900

    #02 toString추가-lee

--- basicgittest/src/com/git/model/vo/Person.java
+++ basicgittest/src/com/git/model/vo/Person.java
@@ -40,6 +40,13 @@ public class Person {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", email=" + email + "]";
+	}
+	
+	
 	
 	
 }
